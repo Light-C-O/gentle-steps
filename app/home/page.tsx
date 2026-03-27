31954822f958d3f3fbfb05fd83c13bebb22bc5a1
Light-C-O <N00242379@iadt.ie>
2026-03-27 16:29:47 +0000

    fixed HomePage()

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 import BulbButton from "@/components/bulb";
 import BooKBackground from "@/components/book-background";
+import LogoIcon from "@/components/logo";
 
 export default function HomePage() {
 
@@ -12,15 +13,19 @@ export default function HomePage() {
   return (
     <main className="flex items-center justify-center mx-auto min-h-screen font-sans overflow-hidden drop-shadow-xl/50">
       <div className="relative w-[90vw] max-w-3xl aspect-3/4 overflow-hidden">
+        
         <BooKBackground/>
         <div className="relative flex flex-col items-center justify-center h-full px-[8%]">
-          <BulbButton/>
-            <h1 className="text-[clamp(28px,4vw,48px)] font-bold mb-4 text-center dark:text-gray-800">Gentle Steps</h1>
-            
-            {/* link to the chapters page */}
-            <Link href="/chapters" className="bg-amber-400 hover:bg-indigo-600 text-2xl text-gray-900 px-4 py-4 rounded-2xl">
-            Open Book 
-            </Link>  
+            <div className="absolute top-10 right-10 sm:top-20 sm:right-20 dark:text-gray-800"><BulbButton/></div>
+            <div className="grid">
+              <h1 className="text-[clamp(28px,4vw,48px)] font-bold mb-4 text-center dark:text-gray-800">Gentle Steps</h1>
+              <div className="justify-self-center w-40 sm:w-100 h-auto mb-5 md:mb-10"><LogoIcon/></div>
+              
+              {/* link to the chapters page */}
+              <Link href="/chapters" className="bg-amber-400 hover:bg-amber-600 text-[clamp(20px,4vw,30px)] text-gray-900 hover:text-gray-100 p-2 sm:p-4 rounded-2xl dark:text-gray-100 text-center dark:hover:bg-indigo-500 dark:bg-blue-500 cursor-pointer">
+              Open Book 
+              </Link> 
+            </div> 
         </div>
       </div>
     </main>
